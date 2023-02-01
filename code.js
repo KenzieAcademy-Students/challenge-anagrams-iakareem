c8f43ec4fa52aaefacd3800a5fe0801b4035357b
ismail Kareem <bkareem2010@gmail.com>
2023-01-31 21:06:12 -0500

    Anagrams - Basic

--- code.js
+++ code.js
@@ -1,1 +1,16 @@
 // Your Code Here.
+const input = document.getElementById("myInput");
+const result = document.getElementById("result");
+
+function findAnagrams() {
+  const sortStr = (str) => {
+    return str.split("").sort().join("");
+  };
+
+  const searchFunctionality = function (words, inputWord) {
+    let filtered = words.filter((word) => sortStr(word) === sortStr(inputWord));
+    result.innerHTML = filtered.join(", ");
+    return filtered;
+  };
+  searchFunctionality(words, input.value);
+}
